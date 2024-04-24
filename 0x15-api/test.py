dbb11fe72cc29dc7d25d113c72ccb20d284cecaf
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-04-24 13:28:43 +0300

    fetching user and todos

--- 0x15-api/test.py
+++ 0x15-api/test.py
@@ -1,48 +1,14 @@
 #!/usr/bin/python3
-
+"""Returns to-do list information for a given employee ID."""
+import requests
 import sys
-import json
-import urllib.request
 
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-base_url = "https://jsonplaceholder.typicode.com"
-
-userId = sys.argv[1]
-todourl = f"{base_url}/todos?userId={userId}"
-user_url = f"{base_url}/users/{userId}"
-
-
-def getuser():
-    response = urllib.request.urlopen(user_url)
-    data = response.read()
-    data_str =  data.decode('utf-8')
-    user = json.loads(data_str)
-    return user['name']
-
-def getTodo():
-
-	todo_response = urllib.request.urlopen(todourl)
-	data_bytes = todo_response.read()
-
-	data_str = data_bytes.decode('utf-8')
-
-	todos = json.loads(data_str)
-
-	total_todos = len(todos)
-	incomplete = 0
-	completed_todos = []
-	for todo in todos:
-		if todo['completed'] is False:
-			incomplete += 1
-			completed_todos.append(todo)
-
-	return (total_todos, incomplete, completed_todos)
-
-
-if __name__ == '__main__':
-	user = getuser()
-	total_todos, incomplete, completed_todos = getTodo()
-
-	print(f"Employee {user} is done with tasks({incomplete}/{total_todos}):")
-	for todo in completed_todos:
-		print(f"\t{todo['title']}",)
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
