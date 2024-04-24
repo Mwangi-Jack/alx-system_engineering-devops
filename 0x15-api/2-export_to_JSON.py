d1d22bb627b4e2825f3183d1f87c91db1d4bd077
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-04-24 16:07:35 +0300

    fetching user and todos and writing the data to a JSON File

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This script fetches todos from an API and writes them
+to a JSON file
+"""
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    userId = sys.argv[1]
+    base_url = "https://jsonplaceholder.typicode.com"
+    user = requests.get(f"{base_url}/users/{userId}",
+                        timeout=10).json()
+    todos = requests.get(f"{base_url}/todos?userId={userId}",
+                         timeout=10).json()
+    username = user.get("username")
+
+    for todo in todos:
+        todo['username'] = username
+
+    data = {user.get('id'): todos}
+
+    with open(f"{userId}.json", "w", encoding="utf-8") as file:
+        file.write(json.dumps(data, indent=4))
