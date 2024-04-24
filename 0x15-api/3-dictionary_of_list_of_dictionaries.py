a7c3e2163d5e935c818ee21468ae21d41900fe2e
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-04-24 17:46:19 +0300

    dictionary list of dictionaries

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This script fetches todos from an API and writes them
+to a JSON file
+"""
+import json
+import requests
+
+if __name__ == "__main__":
+    base_url = "https://jsonplaceholder.typicode.com"
+    todos = requests.get(f"{base_url}/todos",
+                         timeout=10).json()
+
+    userIds = [todo.get("userId") for todo in todos]
+
+    all_data = {}
+
+    for userId in userIds:
+        user = requests.get(f"{base_url}/users/{userId}", timeout=10).json()
+        userTodos = requests.get(f"{base_url}/todos?userId={userId}",
+                                 timeout=10).json()
+        data = [
+            {
+                "username": user.get("username"),
+                "task": todo.get("title"),
+                "completed": todo.get("completed")
+            }
+            for todo in userTodos
+        ]
+
+        all_data[userId] = data
+
+    with open("todo_all_employees.json", "w", encoding='utf-8') as file:
+        file.write(json.dumps(all_data, indent=4))
