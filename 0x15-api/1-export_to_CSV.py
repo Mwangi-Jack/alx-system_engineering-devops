7004a2483d3753f884d8a924837b6bfe6e0f2f78
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-04-24 15:16:25 +0300

    fetching user and todos and writing the data to a csv File

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,16 +7,16 @@ import csv
 import requests
 import sys
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     userId = sys.argv[1]
     base_url = "https://jsonplaceholder.typicode.com"
     user = requests.get(f"{base_url}/users/{userId}",
-                        timeout=10).json()['username']
+                        timeout=10).json()
     todos = requests.get(f"{base_url}/todos?userid={userId}",
                          timeout=10).json()
-
+    username = user.get("username")
     with open(f"{userId}.csv", "w", newline="", encoding="utf-8") as file:
         csv_writer = csv.writer(file, quoting=csv.QUOTE_ALL)
 
         for todo in todos:
-            csv_writer.writerow([userId, user, todo.get("completed"), todo.get("title")])
+            csv_writer.writerow([userId, username, todo.get("completed"), todo.get("title")])
