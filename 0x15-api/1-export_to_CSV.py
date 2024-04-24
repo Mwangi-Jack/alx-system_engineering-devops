1e8ac22fe120f624d120b603f2bd859b0edd93f6
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-04-24 14:59:07 +0300

    fetching user and todos and writing the data to a csv file

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -12,11 +12,11 @@ if __name__ == '__main__':
     todos = requests.get(f"{base_url}/todos?userid={userId}",
                          timeout=10).json()
 
-    with open(f"{userId}.csv", "w", newline="") as file:
+    with open(f"{userId}.csv", "w", newline="", encoding="utf-8") as file:
         csv_writer = csv.writer(file)
-        csv_writer.writerow(todos[0].keys())
+        # csv_writer.writerow(todos[0].keys())
 
         for todo in todos:
-            csv_writer.writerow(todo.values)
-            # file.write("\"{}\", \"{}\", \"{}\", \"{}\"\n".
-                    #    format(userId, user, todo['completed'], todo['title']))
+            # csv_writer.writerow(todo.values())
+            file.write("\"{}\", \"{}\", \"{}\", \"{}\"\n".
+                       format(userId, user, todo['completed'], todo['title']))
