9bcf3125807a85b089a069ce1066986ef1ba2597
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-05-07 12:29:47 +0300

    getting the number of subscribers of a subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -21,5 +21,7 @@ def number_of_subscribers(subreddit):
     if response.status_code == 200:
         data = response.json()
         return data['data']['subscribers']
+    elif response.status_code == 302:
+        return 0
     else:
         return 0
