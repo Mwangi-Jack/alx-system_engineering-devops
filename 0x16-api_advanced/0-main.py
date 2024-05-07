97a9afb5842116d86d1130ec34b872472298bc67
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-05-07 12:45:29 +0300

    getting the number of subscribers of a subreddit

--- 0x16-api_advanced/0-main.py
+++ 0x16-api_advanced/0-main.py
@@ -6,6 +6,7 @@ import sys
 
 if __name__ == '__main__':
     number_of_subscribers = __import__('0-subs').number_of_subscribers
+    print(number_of_subscribers.__doc__)
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-"""
-Qerying Reddit API to get information about
-a subreddit
-"""
 
 import requests
 
