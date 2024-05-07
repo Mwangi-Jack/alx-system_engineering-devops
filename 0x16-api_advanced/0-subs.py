07bf8e8ad7d5d02048264a117583e81d19d9e8e9
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-05-07 16:36:16 +0300

    getting the number of subscribers of a subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,8 +15,7 @@ def number_of_subscribers(subreddit):
     headers = {"User-Agent": "MyApp/1.0"}
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
 
-    response = requests.get(url, timeout=10, headers=headers,
-                            allow_redirects=False)
+    response = requests.get(url, timeout=10, headers=headers)
 
     if response.status_code == 200:
         data = response.json()
