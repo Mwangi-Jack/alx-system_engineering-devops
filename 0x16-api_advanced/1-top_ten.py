9e3ee5b62978bb1ddfbd658d7d6b856b15cf81be
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-05-07 16:29:32 +0300

    getting post titles

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Querying Reddit API to print the title of
+hot posts
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """Qerying API to print title of posts
+
+	Keyword arguments:
+	subreddit -- subreddit to query for
+	Return: title of all subreddit posts or None
+	"""
+
+    url = f'http://www.reddit.com/r/{subreddit}/hot.json'
+    headers = {"User-Agent": "MyApp/1.0"}
+
+    response = requests.get(url, timeout=2, params={"limit": 10}, headers=headers)
+    print(response.status_code)
+    if response.status_code == 200:
+        data = response.json()['data']
+        posts = data['children']
+
+        for post in posts:
+            print(post['data']['title'])
+
+    elif response.status_code == 302:
+        print(None)
+
+    else:
+        print(None)
+
