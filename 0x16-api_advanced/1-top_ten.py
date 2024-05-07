114a14fd82c8b55855ea109877497a9dc2d2d04c
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-05-07 16:47:16 +0300

    getting post titles

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -12,16 +12,19 @@ def top_ten(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
 
     params = {'limit': 10}
+    header = {"User-Agent": "YourApp/1.0"}
 
-    response = requests.get(url,
-                            headers={"User-Agent": "YourApp/1.0"},
+    response = requests.get(url,timeout=5,headers=header,
                             params=params, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()
 
-        for post in data['data']['children']:
+        for i, post in enumerate(data['data']['children']):
             print(post['data']['title'])
+            if i == 9:
+                break
+
     elif response.status_code == 302:
         print('None')
     else:
