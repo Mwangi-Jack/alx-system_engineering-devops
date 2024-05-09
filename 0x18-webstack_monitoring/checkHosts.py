c86837319d1c31c67923a83f38d947945073d1bb
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-05-09 12:42:12 +0300

    setting up datadog dashboard

--- 0x18-webstack_monitoring/checkHosts.py
+++ 0x18-webstack_monitoring/checkHosts.py
@@ -1,5 +1,5 @@
 """
-Get all hosts for your organization returns "OK" response
+Get the total number of active hosts returns "OK" response
 """
 
 from datadog_api_client import ApiClient, Configuration
@@ -8,9 +8,6 @@ from datadog_api_client.v1.api.hosts_api import HostsApi
 configuration = Configuration()
 with ApiClient(configuration) as api_client:
     api_instance = HostsApi(api_client)
-    response = api_instance.list_hosts(
-        filter="env:ci",
-    )
+    response = api_instance.get_host_totals()
 
     print(response)
-
